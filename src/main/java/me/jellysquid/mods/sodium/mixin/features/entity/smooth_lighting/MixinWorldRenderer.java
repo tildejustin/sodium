2d2f5fcd6cf80bfdb81437cc0d825aebbcd386d9
tildejustin <tildejustin@gmail.com>
2026-02-23 14:31:17 -0500

    show a warning on blockentity profiler if entity culling is off

--- src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/smooth_lighting/MixinWorldRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/features/entity/smooth_lighting/MixinWorldRenderer.java
@@ -1,21 +1,30 @@
 package me.jellysquid.mods.sodium.mixin.features.entity.smooth_lighting;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.Objects;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V"))
-    void doNothing(Profiler profiler, String location) {
-        if (!Objects.equals(location, "blockentities") || !SodiumWorldRenderer.getInstance().getUseEntityCulling()) {
-            profiler.swap(location);
+    @WrapWithCondition(method = "render", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=blockentities"))
+    private boolean doNothing(Profiler profiler, String location) {
+        if (!SodiumWorldRenderer.getInstance().getUseEntityCulling()) {
+            return true;
         }
+        profiler.swap("no blockentities (entity culling is off)");
+        profiler.pop();
+        return false;
+    }
+
+    @WrapWithCondition(method = "render", at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=destroyProgress"))
+    private boolean doNothing2(Profiler profiler, String location) {
+        if (!SodiumWorldRenderer.getInstance().getUseEntityCulling()) {
+            return true;
+        }
+        profiler.push(location);
+        return false;
     }
 }
-
